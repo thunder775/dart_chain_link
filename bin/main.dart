b9f697177df5043ec8ddbad3a09c64d61769a923
Mark McLaren <mark.mclaren@mclarencollege.in>
2019-09-27 02:20:50 +0100

    first commit

---
+++ bin/main.dart
@@ -0,0 +1,27 @@
+// In these exercises we will pretend that we do not have access to the standard
+// List class in Dart. Instead, we will implement our own list using 'links' as
+// described below.
+
+// Exercise 1: Create a class called 'Link' which has two fields.  Choose
+// types for each field so that you can create an object structure that looks
+// like this:
+// https://s1.whiteboardfox.com/s/2ae37dc832647bad.png
+
+
+
+
+main() {
+  // Exercise 2:  Create the object structure shown in the above link
+  // including the variable called 'firstLink'
+
+}
+
+
+// Exercise 3: Create a function which takes a Link parameter and prints out
+// all the values in the list.  For example, if the function is given the
+// firstLink variable, then it will print:
+// Raj
+// Makiko
+// Paul
+
+
